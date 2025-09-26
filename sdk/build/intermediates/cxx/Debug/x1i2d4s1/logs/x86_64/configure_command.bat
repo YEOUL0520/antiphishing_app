afd242cbc7d420ba6248e105b13dcca6a66bddac
HyeonSoo Choi <alanghs1111@gmail.com>
2025-09-26 15:39:47 +0900

    OCR 실행하는 함수 추가

--- sdk/build/intermediates/cxx/Debug/x1i2d4s1/logs/x86_64/configure_command.bat
+++ sdk/build/intermediates/cxx/Debug/x1i2d4s1/logs/x86_64/configure_command.bat
@@ -5,15 +5,15 @@
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=21" ^
   "-DANDROID_PLATFORM=android-21" ^
-  "-DANDROID_ABI=x86" ^
-  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
   "-DANDROID_NDK=C:\\Users\\alang\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
   "-DCMAKE_ANDROID_NDK=C:\\Users\\alang\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
   "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\alang\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=C:\\Users\\alang\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\git clone\\sdk\\build\\intermediates\\cxx\\Debug\\x1i2d4s1\\obj\\x86" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\git clone\\sdk\\build\\intermediates\\cxx\\Debug\\x1i2d4s1\\obj\\x86" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\git clone\\sdk\\build\\intermediates\\cxx\\Debug\\x1i2d4s1\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\git clone\\sdk\\build\\intermediates\\cxx\\Debug\\x1i2d4s1\\obj\\x86_64" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BC:\\git clone\\sdk\\.cxx\\Debug\\x1i2d4s1\\x86" ^
+  "-BC:\\git clone\\sdk\\.cxx\\Debug\\x1i2d4s1\\x86_64" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
